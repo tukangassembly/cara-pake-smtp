04a5267eee13dd5f3da5efac3954195d676cd415
Evan Asher <evan.asher@student.umn.ac.id>
2019-01-07 17:53:16 +0700

    Initial Commit

---
+++ SMTPExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SMTPExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
